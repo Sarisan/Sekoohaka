9ccb718b7061e3f85bc1c6804f0ce397f7083f5e
Maria Lisina <sekoohaka.sarisan@gmail.com>
2024-10-07 00:44:13 +0300

    modules/uncache: Add new module
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

---
+++ modules/uncache.sh
@@ -0,0 +1,24 @@
+# Copyright (C) 2024 Maria Lisina
+# Copyright (C) 2024 Danil Lisin
+# SPDX-License-Identifier: Apache-2.0
+
+set -- $(ls -x "${cache}")
+
+while [ ${#} -ge 1 ]
+do
+    until mkdir "${cache}/${1%.*}.lock" > /dev/null 2>&1
+    do
+        sleep 1
+    done
+
+    cache_ctime=$(date +%s)
+    cache_mtime=$(stat -c %Y "${cache}/${1}")
+
+    if [ $((cache_ctime - cache_mtime)) -gt $((caching_time + 10)) ]
+    then
+        rm -f "${cache}/${1}"
+    fi
+
+    rm -fr "${cache}/${1%.*}.lock"
+    shift
+done
