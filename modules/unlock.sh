270787867925f03b7b714a9539d2d1373709788a
Maria Lisina <sekoohaka.sarisan@gmail.com>
2024-10-12 11:58:30 +0300

    modules/unlock: Prevent high load
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- modules/unlock.sh
+++ modules/unlock.sh
@@ -2,4 +2,11 @@
 # Copyright (C) 2024 Danil Lisin
 # SPDX-License-Identifier: Apache-2.0
 
+if ! mkdir "${cache}.unlock" > /dev/null 2>&1
+then
+    exit 0
+fi
+
 find "${cache}" "${config}" -follow -name "*.lock" -type d -mmin +1 -exec rm -fr {} +
+
+rm -fr "${cache}.unlock"
