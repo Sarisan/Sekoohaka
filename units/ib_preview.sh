55b4d230b8f9f0556b817259afb9aa00375090e5
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-02-19 13:58:11 +0300

    units/ib_meta: Include preview fix for non valid formats
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

---
+++ units/ib_preview.sh
@@ -0,0 +1,13 @@
+# Copyright (C) 2024-2025 Maria Lisina
+# Copyright (C) 2024-2025 Danil Lisin
+# SPDX-License-Identifier: Apache-2.0
+
+if [ "${ib_preview_url}" = "${ib_sample_url}" ]
+then
+    ib_preview_url="${ib_error_url}"
+    ib_width="${ib_error_width}"
+    ib_height="${ib_error_height}"
+fi
+
+ib_sample_url="${ib_preview_url}"
+ib_type="photo"
