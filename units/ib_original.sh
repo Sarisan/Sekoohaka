ef94b2189d6e871d14ca7c2c97e0a270bd6b373a
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-02-24 14:54:53 +0300

    units/ib_original: Use ib_meta for meta
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- units/ib_original.sh
+++ units/ib_original.sh
@@ -4,24 +4,12 @@
 
 ib_file_size="$(jq -r ".${ib_iarray}[0].${ib_isize}" "${ib_file}")"
 ib_file_url="$(jq -r ".${ib_iarray}[0].${ib_ifile}" "${ib_file}")"
-ib_preview_url="$(jq -r ".${ib_iarray}[0].${ib_ipreview}" "${ib_file}")"
 
 . "${units}/ib_size.sh"
 
 if [ -n "${ib_file_url}" ] && [ "${ib_file_url}" != "null" ]
 then
-    if [ "${ib_name}" = "Idol Complex" ]
-    then
-        ib_file_url="https:${ib_file_url}"
-    fi
-
-    if [ -z "${ib_preview_url}" ] || [ "${ib_preview_url}" = "null" ]
-    then
-        ib_preview_url=${ib_error_url}
-    elif [ "${ib_name}" = "Idol Complex" ]
-    then
-        ib_preview_url="https:${ib_preview_url}"
-    fi
+    . "${units}/ib_meta.sh"
 else
     output_text="No original file found"
     return 0
