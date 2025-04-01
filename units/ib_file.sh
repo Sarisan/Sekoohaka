7c648d647390a2103749b8d0bd557156bab46f34
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-04-01 11:38:22 +0300

    units/ib_file: Add verbosity
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- units/ib_file.sh
+++ units/ib_file.sh
@@ -71,9 +71,11 @@ then
     output_title="An error occurred"
     output_text="Failed to access ${ib_name} API"
     notification_text="${output_text}"
+    log_text="ib_file: (${update_id}): ${output_text}"
 
     next_offset=${inline_page:-0}
 
+    . "${units}/log.sh"
     rm -fr "${ib_file}" "${cache}/${ib_hash}.lock"
     return 0
 fi
@@ -83,7 +85,9 @@ then
     output_title="An error occurred"
     output_text="An unknown error occurred"
     notification_text="${output_text}"
+    log_text="ib_file: (${update_id}): ${output_text}"
 
+    . "${units}/log.sh"
     rm -fr "${ib_file}" "${cache}/${ib_hash}.lock"
     return 0
 fi
