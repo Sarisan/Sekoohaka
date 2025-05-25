d2e1ccd747d7d1360fc831b9d85b423ca807d89f
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-05-25 16:12:49 +0500

    bot: Mark new version as in active developement
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- bot.zsh
+++ bot.zsh
@@ -10,7 +10,12 @@ set -e
 umask 77
 exec 2> /dev/null
 
-version="4.0.6"
+if command -v git > /dev/null
+then
+    gitrev="$(git rev-parse --short HEAD)"
+fi
+
+version="4.1-${gitrev:-inadev}"
 useragent="Sekoohaka/${version} Telegram Bot"
 
 local_address="127.0.0.1:8081"
