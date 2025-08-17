20a58dd7a329af1ccf8528bab63c5f47a97fb583
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-08-17 14:56:35 +0500

    submodules/command_source: Refactor text geneartion
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- submodules/command_source.zsh
+++ submodules/command_source.zsh
@@ -28,6 +28,15 @@ then
     return 0
 fi
 
+source_table=(
+    "Danbooru" d danbooru_id
+    "Gelbooru" g gelbooru_id
+    "Idol Complex" i idol_id
+    "Konachan.com" k konachan_id
+    "Sankaku Channel" s sankaku_id
+    "yande.re" y yandere_id
+)
+
 minimum_similarity="$(jq -r ".header.minimum_similarity" "${sn_file}")"
 highest_similarity=0
 highest_index=0
@@ -61,44 +70,25 @@ link_preview_options="$(
         '{"url": $url, "prefer_small_media": true, "show_above_text": true}'
 )"
 
-danbooru_id="$(jq -r ".results.[${highest_index}].data.danbooru_id" "${sn_file}")"
-gelbooru_id="$(jq -r ".results.[${highest_index}].data.gelbooru_id" "${sn_file}")"
-idol_id="$(jq -r ".results.[${highest_index}].data.idol_id" "${sn_file}")"
-konachan_id="$(jq -r ".results.[${highest_index}].data.konachan_id" "${sn_file}")"
-sankaku_id="$(jq -r ".results.[${highest_index}].data.sankaku_id" "${sn_file}")"
-yandere_id="$(jq -r ".results.[${highest_index}].data.yandere_id" "${sn_file}")"
-
 output_text="$(printf "<b>SauceNAO</b>\n<b>Similarity:</b> %.2f%%" "${highest_similarity}")"
 
-if [[ "${danbooru_id}" != "null" ]]
-then
-    output_text="$(printf "%s\n<b>Danbooru ID:</b> <code>%s</code>" "${output_text}" "${danbooru_id}")"
-fi
+while [[ ${#source_table} -ge 3 ]]
+do
+    ib_id="$(jq -r ".results.[${highest_index}].data.${source_table[3]}" "${sn_file}")"
 
-if [[ "${gelbooru_id}" != "null" ]]
-then
-    output_text="$(printf "%s\n<b>Gelbooru ID:</b> <code>%s</code>" "${output_text}" "${gelbooru_id}")"
-fi
+    if [[ "${ib_id}" == "null" ]]
+    then
+        shift 3 source_table
+        break
+    fi
 
-if [[ "${idol_id}" != "null" ]]
-then
-    idol_md5="$(jq -r ".results.[${highest_index}].header.index_name" "${sn_file}" | cut -d ' ' -f 6 | cut -d '_' -f 1)"
+    output_text="$(printf "%s\n<b>%s ID:</b> <code>%s</code>" "${output_text}" "${source_table[1]}" "${ib_id}")"
 
-    output_text="$(printf "%s\n<b>Idol Complex ID:</b> <code>%s</code>" "${output_text}" "${idol_id}")"
-    output_text="$(printf "%s\n<b>Idol Complex MD5:</b> <code>%s</code>" "${output_text}" "${idol_md5}")"
-fi
+    if [[ "${source_table[3]}" == "idol_id" ]]
+    then
+        ib_md5="$(jq -r ".results.[${highest_index}].header.index_name" "${sn_file}" | cut -d ' ' -f 6 | cut -d '_' -f 1)"
+        output_text="$(printf "%s\n<b>%s MD5:</b> <code>%s</code>" "${output_text}" "${source_table[1]}" "${ib_md5}")"
+    fi
 
-if [[ "${konachan_id}" != "null" ]]
-then
-    output_text="$(printf "%s\n<b>Konachan.com ID:</b> <code>%s</code>" "${output_text}" "${konachan_id}")"
-fi
-
-if [[ "${sankaku_id}" != "null" ]]
-then
-    output_text="$(printf "%s\n<b>Sankaku Channel ID:</b> <code>%s</code>" "${output_text}" "${sankaku_id}")"
-fi
-
-if [[ "${yandere_id}" != "null" ]]
-then
-    output_text="$(printf "%s\n<b>yande.re ID:</b> <code>%s</code>" "${output_text}" "${yandere_id}")"
-fi
+    shift 3 source_table
+done
