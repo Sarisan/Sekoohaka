7604fd62bd01823e7187318beedab8faac42650c
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-08-18 10:42:16 +0500

    submodules/command_parent: Add quick button
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- submodules/command_parent.zsh
+++ submodules/command_parent.zsh
@@ -17,8 +17,18 @@ then
     return 0
 fi
 
-ib_children_ids=($(jq -r ".${ib_iarray}[].${ib_iid}" "${ib_file}"))
+keyboard_text1="Open inline"
+keyboard_query1="posts ${ib_board} parent:${ib_post_id}"
+
+reply_markup="$(
+    jq --null-input --compact-output \
+        --arg text1 "${keyboard_text1}" \
+        --arg query1 "${keyboard_query1}" \
+        '.inline_keyboard[0] += [{"text": $text1, "switch_inline_query_current_chat": $query1}]'
+)"
+
 output_text="$(printf "<b>Children IDs:</b>")"
+ib_children_ids=($(jq -r ".${ib_iarray}[].${ib_iid}" "${ib_file}"))
 
 for children_id in ${ib_children_ids[@]}
 do
