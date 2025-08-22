1b4a58cdfc6086e5adae88ebe48aa2920f708124
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-08-22 13:47:27 +0500

    submodules/command_shorts: Refactor if statemnet
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- submodules/command_shorts.zsh
+++ submodules/command_shorts.zsh
@@ -14,37 +14,40 @@ then
     shorts=($(ls -1 "${shorts_config}"))
 fi
 
-if [[ ${#shorts} -gt 0 ]]
+if [[ ${#shorts} -eq 0 ]]
 then
-    output_text="<b>Saved shortcuts:</b> ${#shorts} / ${shorts_limit}"
+    output_text="You have no saved shortcuts yet"
 
-    keyboard_text1="Open inline"
-    keyboard_query1="shorts "
+    rmdir "${user_config}_shorts.lock"
+    return 0
+fi
+
+output_text="<b>Saved shortcuts:</b> ${#shorts} / ${shorts_limit}"
+
+keyboard_text1="Open inline"
+keyboard_query1="shorts "
+
+reply_markup="$(
+    jq --null-input --compact-output \
+        --arg text1 "${keyboard_text1}" \
+        --arg query1 "${keyboard_query1}" \
+        --arg text2 "${keyboard_text2}" \
+        --arg data2 "${keyboard_data2}" \
+        '{"inline_keyboard": [[{"text": $text1, "switch_inline_query_current_chat": $query1}]]}'
+)"
+
+if [[ "${chat_id}" == "${target_user}" ]]
+then
+    keyboard_text1="Remove all"
+    keyboard_data1="stop shorts"
 
     reply_markup="$(
-        jq --null-input --compact-output \
+        jq --compact-output \
             --arg text1 "${keyboard_text1}" \
-            --arg query1 "${keyboard_query1}" \
-            --arg text2 "${keyboard_text2}" \
-            --arg data2 "${keyboard_data2}" \
-            '{"inline_keyboard": [[{"text": $text1, "switch_inline_query_current_chat": $query1}]]}'
+            --arg data1 "${keyboard_data1}" \
+            '.inline_keyboard[0] += [{"text": $text1, "callback_data": $data1}]' \
+        <<< "${reply_markup}"
     )"
-
-    if [[ "${chat_id}" == "${target_user}" ]]
-    then
-        keyboard_text1="Remove all"
-        keyboard_data1="stop shorts"
-
-        reply_markup="$(
-            jq --compact-output \
-                --arg text1 "${keyboard_text1}" \
-                --arg data1 "${keyboard_data1}" \
-                '.inline_keyboard[0] += [{"text": $text1, "callback_data": $data1}]' \
-            <<< "${reply_markup}"
-        )"
-    fi
-else
-    output_text="You have no saved shortcuts yet"
 fi
 
 rmdir "${user_config}_shorts.lock"
