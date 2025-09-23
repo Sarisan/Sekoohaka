4cb1f8b5402c28c733cba9054ceb86852eda7b27
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-09-23 11:53:20 +0500

    submodules/command_source: Fix Sankaku Channel MD5 field
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- submodules/command_source.zsh
+++ submodules/command_source.zsh
@@ -82,9 +82,15 @@ do
 
     output_text="$(printf "%s\n<b>%s ID:</b> <code>%s</code>" "${output_text}" "${source_table[3]}" "${ib_id}")"
 
-    if [[ "${source_table[1]}" == "idol_id" || "${source_table[1]}" == "sankaku_id" ]]
+    if [[ "${source_table[1]}" == "idol_id" ]]
     then
         ib_id="$(jq -r ".results.[${highest_index}].header.index_name" <<< "${sn_data}" | cut -d ' ' -f 6 | cut -d '_' -f 1)"
+        output_text="$(printf "%s\n<b>%s MD5:</b> <code>%s</code>" "${output_text}" "${source_table[3]}" "${ib_id}")"
+    fi
+
+    if [[ "${source_table[1]}" == "sankaku_id" ]]
+    then
+        ib_id="$(jq -r ".results.[${highest_index}].header.index_name" <<< "${sn_data}" | cut -d ' ' -f 5 | cut -d '_' -f 1)"
         output_text="$(printf "%s\n<b>%s MD5:</b> <code>%s</code>" "${output_text}" "${source_table[3]}" "${ib_id}")"
     fi
 
