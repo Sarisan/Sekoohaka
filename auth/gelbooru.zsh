7757a850669b4b1a1dff1bf4f6500f6b3be6675f
Maria Lisina <sekoohaka.sarisan@gmail.com>
2025-10-06 04:08:14 +0500

    auth/gelbooru: Fix authorization
    Signed-off-by: Maria Lisina <sekoohaka.sarisan@gmail.com>

--- auth/gelbooru.zsh
+++ auth/gelbooru.zsh
@@ -3,3 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 output_text="Authorization cannot be verified, make sure you provided correct credentials"
+
+printf "%s\n" "${ib_login}" > "${login_file}"
+printf "%s\n" "${ib_key}" > "${key_file}"
